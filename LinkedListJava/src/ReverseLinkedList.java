ea0f2e875f87f678e52110d12d9c74274c3368ee
Babu Chandermani Rawat <BabuChandermaniRawat@users.noreply.github.com>
2021-05-25 11:15:33 +0530

    ReverseLinkedList
    Java Code to reverse a linked list.

---
+++ LinkedListJava/src/ReverseLinkedList.java
@@ -0,0 +1,47 @@
+
+public class ReverseLinkedList {
+	static Node head;
+	//Node creation
+    static class Node{
+    	int data;
+    	Node next;
+    	
+    	Node(int data){
+    		this.data = data;
+    		this.next = null;
+    	}
+    }
+    static Node reverseList(Node head) {
+    	Node prev = null;
+    	Node current = head;
+    	Node next = null;
+    	while(current != null) {
+    		next = current.next;
+    		current.next = prev;
+    		prev = current;
+    		current = next;
+    	}
+    	head = prev;
+    	return head;
+    }
+    //Print the linkedList
+    static void printlist(Node node) {
+    	while(node != null) {
+    		System.out.print(node.data+" ");
+    		node = node.next;
+    	}
+    	System.out.println();
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReverseLinkedList reverse = new ReverseLinkedList();
+		reverse.head = new Node(85);
+		reverse.head.next = new Node(15);
+		reverse.head.next.next = new Node(4);
+		reverse.head.next.next.next = new Node(20);
+		reverse.printlist(head);
+		head = reverse.reverseList(head);
+        reverse.printlist(head);
+	}
+
+}
